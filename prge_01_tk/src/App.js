b848fa44ee54976131ee006b78427bfe7b249ead
markos787 <149382126+markos787@users.noreply.github.com>
2024-04-04 12:53:55 +0200

    wip:initial_setup

---
+++ prge_01_tk/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const lista_uzytkownikow = [
+  { imie: "Marek" },
+  { imie: "Wika" },
+  { imie: "Kinia" },
+];
+
+console.log(lista_uzytkownikow);
+
+function App() {
+  return (
+    <div className="App">
+      {lista_uzytkownikow.map((zolnierz) => {
+        return <div>{zolnierz.imie}</div>;
+      })}
+    </div>
+  );
+}
+
+export default App;
